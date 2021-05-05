8de825c8add41714c494fb4ae1227fa9d15f3025
Joshua Rose <jlr007@protonmail.com>
2021-05-05 15:49:36 -0400

    beta version of personal web site complete...not yet built for ng deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ResumeComponent } from './components/resume/resume.component';
+import { SocialComponent } from './components/social/social.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'about' },
   { path: 'about', component: HomeComponent },
-  { path: 'contact', component: ContactComponent },
+  { path: 'social', component: SocialComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'resume', component: ResumeComponent },
   { path: '**', component: NotFoundComponent }
